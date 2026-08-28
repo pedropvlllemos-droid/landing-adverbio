102a8be92f653269d7012b79ba5996e2731026ad
pedropvlllemos-droid <pedrop.vll.lemos@gmail.com>
2026-08-28 16:40:18 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 // 2) Cole abaixo a URL terminada em /exec.
 // 3) Troque os números pelos WhatsApps reais, sempre com 55 + DDD + número, somente dígitos.
 window.ADVERBIO_CONFIG = {
-  appsScriptUrl: "COLE_AQUI_A_URL_DO_APPS_SCRIPT",
+  appsScriptUrl: https://script.google.com/macros/s/AKfycbyjK6CVyNk0nJ51B4J3X-V2RdsiguPBCAc2Mchs7AWcnRqsMKxJi39PbUKU7ec_1GZ1/exec,
   whatsapp: {
-    high: "5562999999999",
-    business: "5562888888888",
-    starter: "5562777777777"
+    high: "5562992412210",
+    business: "5562992412210",
+    starter: "5562992412210"
   }
 };
